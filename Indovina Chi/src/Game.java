e466e72c6dfa3618ae90f983a3ba517628a53715
Paolo Ficara <paoloficara50@gmail.com>
2021-12-15 18:55:46 +0100

    mettendo basi

--- Indovina Chi/src/Game.java
+++ Indovina Chi/src/Game.java
@@ -1,57 +1,93 @@
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author ficara_paolo
  */
 public class Game extends Canvas implements Runnable {
-    
+
     private static final long serialVersionUID = -240840600533728354L;
     private boolean running = false;
-    
-    public Game(){
+    private Thread thread;
+
+    public Game() {
         new Window(1280, 720, "Indovina chi?", this);
     }
-    
-    public synchronized void start(){
-        
+
+    public synchronized void start() {
+        thread = new Thread(this);
+        thread.start();
+        running = true;
     }
-    
-    public void run(){
+
+    public synchronized void stop() {
+        try {
+            thread.join();
+            running = false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
         long lastTime = System.nanoTime();
         double amountOfTicks = 60.0;
         double ns = 1000000000 / amountOfTicks;
         double delta = 0;
         long timer = System.currentTimeMillis();
         int frames = 0;
-        while(running){
+        while (running) {
             long now = System.nanoTime();
             delta += (now - lastTime) / ns;
             lastTime = now;
-            while(delta >= 1){
+            while (delta >= 1) {
                 tick();
                 delta--;
             }
-            if(running)
+            if (running) {
                 render();
+            }
             frames++;
-            if(System.currentTimeMillis() - timer > 1000){
+            if (System.currentTimeMillis() - timer > 1000) {
                 timer += 1000;
                 System.out.println("FPS: " + frames);
-                frames = 0
+                frames = 0;
             }
         }
-        stoop();
+        stop();
     }
+
+    private void tick() {
+
+    }
+
+    private void render() {
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null) {
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        
+        g.setColor(Color.black);
+        g.fillRect(0, 0, 1280, 720);
+        
+        g.dispose();
+        bs.show();
+    }
+
     public static void main(String[] args) {
         new Game();
     }
-    
+
 }
